8aed33491bffc976c666403b52d0aea24d4ef3bb
Mhirii <ahmedmhiri218@gmail.com>
2024-01-19 03:39:12 +0100

    smart panes

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = {}
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 -- ── Binds ────────────────────────────────────
@@ -14,8 +14,17 @@ config.keys = binds.keys
 -- ── UI ───────────────────────────────────────
 local ui = require("ui")
 for key, value in pairs(ui) do
-	config[key] = value
+  config[key] = value
 end
+
+local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
+smart_splits.apply_to_config(config, {
+  direction_keys = { "h", "j", "k", "l" },
+  modifiers = {
+    move = "CTRL", -- modifier to use for pane movement, e.g. CTRL+h to move left
+    resize = "ALT", -- modifier to use for pane resize, e.g. META+h to resize to the left
+  },
+})
 
 -- ── Hyprlinks ────────────────────────────────
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
@@ -25,8 +34,8 @@ config.hyperlink_rules = wezterm.default_hyperlink_rules()
 -- as long as a full url hyperlink regex exists above this it should not match a full url to
 -- github or gitlab / bitbucket (i.e. https://gitlab.com/user/project.git is still a whole clickable url)
 table.insert(config.hyperlink_rules, {
-	regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
-	format = "https://www.github.com/$1/$3",
+  regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+  format = "https://www.github.com/$1/$3",
 })
 
 return config
