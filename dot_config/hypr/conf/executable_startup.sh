d970137b76bade2df9eb789f3f6818ab9412fd39
Mhirii <ahmedmhiri218@gmail.com>
2024-04-21 20:44:36 +0100

    Update .config/hypr/conf/binds.conf Update .config/hypr/conf/decoration.conf Update .config/hypr/conf/startup.sh Update .config/hypr/conf/rules.conf Update .config/hypr/conf/startup.conf Add .config/hypr/hyprpaper.conf Add .config/hypr/themes/hypr/colors Add .config/hypr/themes/hypr/hypr.json Add .config/hypr/themes/hypr/tokyonight.css Add .config/hypr/themes/hypr/tokyonight.ini Add .config/hypr/wallpapers/china.png Add .config/hypr/wallpapers/cyan_tokyo.png Update .config/hypr/wallpapers/tokyo.png Add .config/hypr/wallpapers/tokyo_girl.png

--- dot_config/hypr/conf/executable_startup.sh
+++ dot_config/hypr/conf/executable_startup.sh
@@ -15,10 +15,10 @@ echo_color() {
 
 echo_color "Starting up..." "green"
 
-echo_color "Setting up wallpaper..." "cyan"
-swww init
-swww img --transition-type grow ~/.config/hypr/background.png
-echo_color "Wallpaper set!" "green"
+# echo_color "Setting up wallpaper..." "cyan"
+# swww init
+# swww img --transition-type grow ~/.config/hypr/background.png
+# echo_color "Wallpaper set!" "green"
 
 # echo_color "Setting up widgets..." "cyan"
 # ags -t bar &
@@ -37,19 +37,13 @@ echo_color "Starting up clickup..." "blue"
 clickup &
 echo_color "done!" "green"
 
-# warp-terminal &
-# hyprctl dispatch focuswindow "dev.warp.Warp"
-# hyprctl dispatch toggleFloating "dev.warp.Warp"
-# hyprctl dispatch movetoworkspacesilent 1
-
 hyprctl dispatch exec [workspace special silent] "alacritty -e ~/scripts/tmuxMain.sh"
 hyprctl dispatch exec [workspace 8 silent] alacritty
 
-# if battery level > 50% then echo yes, the command acpi -b returns Battery 0: Charging, 76%, 00:26:32 until charged
 if [ "$(acpi -b | awk '{print $4}' | tr -d '%,')" -gt 50 ]; then
 	hyprctl dispatch exec [workspace 2 silent] "$HOME/scripts/launch.fish" vivaldi
 	hyprctl dispatch exec [workspace 5 silent] webcord
-	hyprctl dispatch exec [workspace 6 silent] spotify
+	hyprctl dispatch exec [workspace 6 silent] "$HOME/scripts/launch.fish" spotify
 fi
 
 echo_color "Applications started!" "green"
