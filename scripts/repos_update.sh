1f124dbb6da97578c0d3c06473ebc59bb15344ef
Mhirii <ahmedmhiri218@gmail.com>
2024-07-13 16:58:23 +0100

    add colors to script

--- scripts/repos_update.sh
+++ scripts/repos_update.sh
@@ -6,9 +6,26 @@ if [ -n "$1" ] && [ "$1" = "pull" ]; then
 fi
 
 repos=$(cd && fd config --hidden --exclude ".local" --exclude ".cache" --exclude ".tmux" | rg .git/config)
+
+failed_repos=()
 for r in $repos; do
+  echo
   dir="$HOME/${r%/.git/config}"
-  echo "Updating $dir"
-  cd "$dir" || exit
+  echo -e "\e[33m  Updating $dir\e[0m"
+  cd "$dir" || continue
   $command
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m  SUCCESS\e[0m"
+  else
+    echo -e "\e[31m  FAILURE\e[0m"
+    failed_repos+=("$dir")
+  fi
+  echo
 done
+
+if [ ${#failed_repos[@]} -ne 0 ]; then
+  echo -e "\e[31mFailed repositories:\e[0m"
+  for repo in "${failed_repos[@]}"; do
+    echo "  $repo"
+  done
+fi
